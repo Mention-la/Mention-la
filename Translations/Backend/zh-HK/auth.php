6897b6ba65cb0916d99c0d1018cc33e4df7c23a8
Johnny Tsoi <hallelujahbaby@gmail.com>
2020-01-03 18:34:41 +0800

    Add files via upload

---
+++ Translations/Backend/zh-HK/auth.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed'   => '用戶名或密碼錯誤',
+    'throttle' => '登入次數過多，請在 :seconds 秒後再試。',
+    'invalid_token' => '存取憑證無效。',
+    'password_fail' => '密碼錯誤。',
+    'password_reset' => '成功重設密碼。',
+    'password_reset_fail' => '重設密碼失敗。',
+    'fail_email' => '電郵地址不正確。',
+    'reset_email_sent' => '重設密碼電郵已發送。',
+];
---
+++ Translations/Backend/zh-Hant/auth.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed'   => '使用者名稱或密碼錯誤',
+    'throttle' => '嘗試登入太多次，請在 :seconds 秒後再試。',
+    'invalid_token' => '存取權杖無效。',
+    'password_fail' => '密碼錯誤。',
+    'password_reset' => '成功重設密碼。',
+    'password_reset_fail' => '重設密碼失敗。',
+    'fail_email' => '電郵地址錯誤。',
+    'reset_email_sent' => '重設密碼電郵已發送。',
+];
